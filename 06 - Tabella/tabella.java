201a131b79e82410b460541905ce163ed58d2fd6
yvngshvdoo <148055968+yvngshvdoo@users.noreply.github.com>
2023-10-19 22:36:33 +0200

    Add files via upload

---
+++ 06 - Tabella/tabella.java
@@ -0,0 +1,21 @@
+package tabella;
+
+public class tabella {
+	
+	public static void main(String[] args) {
+		/*
+		System.out.println("Persona Altezza(cm)\tMisura di scarpe");
+		System.out.println("=====================================");
+		System.out.println("Luisa\t160\t\t36");
+		System.out.print("Marco\t180\t\t43\n");
+		System.out.println("Giulia\t168\t\t38");
+	*/
+	System.out.println("Persona\t\tAltezza(cm)\tMisura di scarpe\n"+
+						"=====================================\n"+
+						"Luisa\t160\t\t36\n"+
+						"Marco\t180\t\t43\n"+
+						"Giulia\t168\t\t38");
+	}
+						
+	
+}
